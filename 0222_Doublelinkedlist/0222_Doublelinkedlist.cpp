b54fb7f3482de0c358edb8eac90dd859f906f3f8
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-03 08:49:29 +0700

    Step 1.e, memindahkan arus ke node berikutnya

--- 0222_Doublelinkedlist/0222_Doublelinkedlist.cpp
+++ 0222_Doublelinkedlist/0222_Doublelinkedlist.cpp
@@ -49,6 +49,7 @@ void addNode()
         while (current != NULL && current->noMhs < newNode->noMhs)
         {
             previous = current;
+            current = current->next;
         }
     }
 }
