1488952c04323f15741ae98c66cd1b0d088affdb
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-03 08:45:46 +0700

    Step 1.c, melintasi daftar untuk menemukan posisi yang benar

--- 0222_Doublelinkedlist/0222_Doublelinkedlist.cpp
+++ 0222_Doublelinkedlist/0222_Doublelinkedlist.cpp
@@ -45,6 +45,11 @@ void addNode()
         
         Node* current = START;
         Node* previous = NULL;
+
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {
+            
+        }
     }
 }
 
