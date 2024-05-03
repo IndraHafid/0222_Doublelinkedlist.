c2c202f8c890505b977d4377fb46497cb8fdee54
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-03 09:34:31 +0700

    Menambahkan pencarian berdasarkan nomor roll

--- 0222_Doublelinkedlist/0222_Doublelinkedlist.cpp
+++ 0222_Doublelinkedlist/0222_Doublelinkedlist.cpp
@@ -84,7 +84,7 @@ bool search(int rollNo, Node** previous, Node** current)
 
 void deleteNode()
 {
-    Node* prevoius, * current;
+    Node* previous, * current;
     int rollNo;
 
     cout << "\nEnter the roll number off the student whose recird is to be deleted: ";
@@ -99,8 +99,18 @@ void deleteNode()
     current = START;
     previous = NULL;
 
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
 
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number " << rollNo << "not found\033[0m" << endl;
+        return;
 
+    }
 }
 
 int main()
